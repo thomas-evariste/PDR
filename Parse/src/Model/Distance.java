1e818860be814b1838f2f3a3387ff4e9f561afcc
Thomas Guyot <thomas.Evariste.guyot@gmail.com>
2019-05-14 10:06:44 +0200

    mise a jour model

--- Parse/src/Model/Distance.java
+++ Parse/src/Model/Distance.java
@@ -1,80 +1,63 @@
 package Model;
 
+import java.util.*;
+
 public class Distance {
 
-	private int id1;
-	private int id2; // on prend id1 < id2 comme convention
-	private int dist;
+	private int id;
+	private ArrayList<BlocDistance> dists;
 	
 	public Distance(){
-		id1=-1;
-		id2=-1;
-		dist=-1;
+		id=-1;
+		dists= new ArrayList<BlocDistance>();
 	}
 	
-	public Distance(int id1, int id2){
-		this.id1=min(id1,id2);
-		this.id2=max(id1,id2);
-		dist=-1;
+	public Distance(int id){
+		this.id=id;
+		dists= new ArrayList<BlocDistance>();
 	}
 	
-	public Distance(int id1, int id2, int dist){
-		this.id1=min(id1,id2);
-		this.id2=max(id1,id2);
-		this.dist=dist;
+	public Distance(int id, ArrayList<BlocDistance> dists){
+		this.id=id;
+		this.dists=dists;
 	}
 
-	public void setId1(int id){
-		if(id2!=-1){
-			if(id>id2){
-				id1=id2;
-				id2=id;
-			}
-			else{
-				id1=id;
-			}
-		}
-		else{
-			id1=id;
-		}
+	public void setId(int id){
+		this.id=id;
 	}
 
-	public void setId2(int id){
-		if(id1!=-1){
-			if(id<id1){
-				id2=id1;
-				id1=id;
-			}
-			else{
-				id1=id;
+	
+	public void setDists(ArrayList<BlocDistance> dists){
+		this.dists=dists;
+	}
+	
+	public void add(BlocDistance dist){
+		dists.add(dist);
+	}
+	
+	public ArrayList<BlocDistance> getDists(){
+		return dists;
+	}
+	
+	public BlocDistance getDist(int num){
+		return dists.get(num);
+	}
+	
+	public BlocDistance getDistByDistance(int distance){
+		for(BlocDistance dist : dists){
+			if(dist.getDistance()==distance){
+				return dist;
 			}
 		}
-		else{
-			id1=id;
-		}
-	}
-	
-	public void setIds(int id1, int id2){
-		this.id1=min(id1,id2);
-		this.id2=max(id1,id2);
-	}
-	
-	public void setDist(int dist){
-		this.dist=dist;
-	}
-	
-	public int getDist(){
+		BlocDistance dist = new BlocDistance();
+		dist.setDistance(distance);
+		dists.add(dist);
 		return dist;
 	}
 	
-	public int getId1(){
-		return id1;
+	public int getId(){
+		return id;
 	}
-	
-	public int getId2(){
-		return id2;
-	}
-	
 	
 	
 	
